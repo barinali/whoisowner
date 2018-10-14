4923708285c7c47b4632e1b463fe5871dab5ebd3
Hasan Tayyar BEŞİK <tayyar.besik@gmail.com>
2018-10-14 21:09:01 +0200

    I do not believe in modern society but
    but I believe in modern js

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -1,34 +1,34 @@
-chrome.runtime.onMessage.addListener(function(msg, sender) {
+chrome.runtime.onMessage.addListener((msg, sender) => {
   if (msg.type == 1) {
     setAssigneers();
   }
 });
 
-var setAssigneers = function() {
-  var $table = document.querySelectorAll('.ghx-sprint-report-table table.aui');
-  Array.prototype.forEach.call($table, function(table) {
-    Array.prototype.forEach.call(table.querySelectorAll('tbody tr'), function(row) {
-      var $row = row;
-      var $firstColumn = $row.firstChild;
-      var $exampleColumn = $firstColumn.nextSibling;
+var setAssigneers = () => {
+  const $table = document.querySelectorAll('.ghx-sprint-report-table table.aui');
+  Array.prototype.forEach.call($table, table => {
+    Array.prototype.forEach.call(table.querySelectorAll('tbody tr'), row => {
+      const $row = row;
+      const $firstColumn = $row.firstChild;
+      const $exampleColumn = $firstColumn.nextSibling;
 
       $assigneeColumn = $exampleColumn.cloneNode();
       $row.insertBefore($assigneeColumn, $exampleColumn);
 
-      var $issueLink = $firstColumn.querySelector('a');
+      const $issueLink = $firstColumn.querySelector('a');
 
       if ($issueLink) {
-        var issueLink = $issueLink.getAttribute('href');
+        const issueLink = $issueLink.getAttribute('href');
 
-        (function(node) {
-          var xhr = new XMLHttpRequest();
+        ((node => {
+          const xhr = new XMLHttpRequest();
 
-          xhr.onreadystatechange = function() {
+          xhr.onreadystatechange = () => {
             if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
-              var $loadedPage = document.implementation.createHTMLDocument().documentElement;
+              const $loadedPage = document.implementation.createHTMLDocument().documentElement;
               $loadedPage.innerHTML = xhr.responseText;
-              var $user = $loadedPage.querySelector('#assignee-val [data-user]');
-              var user = { displayName: '-' };
+              const $user = $loadedPage.querySelector('#assignee-val [data-user]');
+              let user = { displayName: '-' };
 
               if ($user)
                 user = JSON.parse($user.dataset.user)
@@ -39,7 +39,7 @@ var setAssigneers = function() {
           xhr.open("GET", issueLink, true);
           xhr.send();
 
-        })($assigneeColumn);
+        }))($assigneeColumn);
       }
     });
   });
